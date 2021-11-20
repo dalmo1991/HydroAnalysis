401b8c971835a61066bd3fef900f6b9174126cec
Dalmo <marco.dalmolin.1991@gmail.com>
2021-11-20 12:48:18 +0100

    Ready to publish 	new file:   requirements.txt 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+
+setuptools.setup(
+    name='hydroanalysis',
+    version='1.0.0',
+    author='Marco Dal Molin',
+    author_email='marco.dalmolin.1991@gmail.com',
+    description='Framework for calculating hydrological indices',
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       "README.md")).read(),
+    long_description_content_type='text/markdown',
+    url='https://hydroanalysis.readthedocs.io/en/latest/',
+    license='LGPL',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',  # https://martin-thoma.com/software-development-stages/
+        'Topic :: Scientific/Engineering :: Hydrology',
+    ],
+    install_requires=[
+        'numpy>=1.20.*',
+        'pandas>=1.2.*',
+        'scipy>=1.6.*',
+    ],
+)
